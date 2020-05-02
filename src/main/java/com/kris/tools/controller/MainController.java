6a6bf7f280f08df201780771a1486e2ed252b156
KRIS <krisshang@qq.com>
2020-05-02 16:02:29 +0800

    Initial commit

---
+++ src/main/java/com/kris/tools/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.kris.tools.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: KRIS
+ * @Date: 2020/5/2
+ **/
+@RestController
+@RequestMapping("/tools")
+public class MainController {
+
+    @RequestMapping("/home")
+    public String main(){
+        return "Hello World !";
+    }
+}
